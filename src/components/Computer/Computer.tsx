767099fa12e0c9b51d12a22ef25d4d89e3c828df
Jesus Alejandro Ortiz Trejo <117122722+AlexOT03@users.noreply.github.com>
2024-11-09 16:51:03 -0600

    fix prettier format and styles

--- src/components/Computer/Computer.tsx
+++ src/components/Computer/Computer.tsx
@@ -2,53 +2,53 @@ import { Hacker, UserType } from "../Hacker/Hacker";
 import styles from "./Computer.module.css";
 
 interface ComputerProps {
-  user?: UserType;
-  className?: string;
+	user?: UserType;
+	className?: string;
 }
 
 export const Computer = ({ user, className }: ComputerProps) => {
-  return (
-    <div className={`${styles.computer} ${className}`}>
-        <div className={styles.monitor}>
-            <div className={styles.inner_screen}>
-                <div className={styles.screen}>
-                 {user && <Hacker {...user} />}
-                 {!user && <div className={styles.noise} />}
-                </div>
-                <div className={styles.buttons}>
-                    <div className={styles.button_top}></div>
-                    <div className={styles.button_bottom}></div>
-                    <div className={styles.button_bottom}></div>
-                </div>
-            </div>
-        </div>
-        <div className={styles.cpu}>
-            <div className={styles.vent}>
-                <div className={styles.power_button}></div>
-                <div className={styles.vent_lines}>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                    <div className={styles.vent_line}></div>
-                </div>
-            </div>
-            <div className={styles.drives}>
-                <div className={styles.drive}></div>
-                <div className={styles.drive}></div>
-            </div>
-        </div>
-    </div>
-  );
+	return (
+		<div className={`${styles.computer} ${className}`}>
+			<div className={styles.monitor}>
+				<div className={styles.inner_screen}>
+					<div className={styles.screen}>
+						{user && <Hacker {...user} />}
+						{!user && <div className={styles.noise} />}
+					</div>
+					<div className={styles.buttons}>
+						<div className={styles.button_top}></div>
+						<div className={styles.button_bottom}></div>
+						<div className={styles.button_bottom}></div>
+					</div>
+				</div>
+			</div>
+			<div className={styles.cpu}>
+				<div className={styles.vent}>
+					<div className={styles.power_button}></div>
+					<div className={styles.vent_lines}>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+						<div className={styles.vent_line}></div>
+					</div>
+				</div>
+				<div className={styles.drives}>
+					<div className={styles.drive}></div>
+					<div className={styles.drive}></div>
+				</div>
+			</div>
+		</div>
+	);
 };
